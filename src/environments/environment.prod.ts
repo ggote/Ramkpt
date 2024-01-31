f8b140637ab6baee26dfc12b08f7adead0eee5b0
Adesh Shejul <gopalgote26@gmail.com>
2024-01-31 19:10:40 +0530

    resolve prod env build issue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,11 @@
 export const environment = {
   production: true,
-  bdTraceUrl : 'https://pay.billdesk.com/jssdk/v1/dist/',
-  baseUrl: ''
+  bdTraceUrl: 'https://uat.billdesk.com/jssdk/v1/dist/',
+  baseUrl: 'https://pguat.billdesk.io/',
+  alg: "HS256",
+  clientId : "uatramkmcc",
+  merchandId: "UATRAMKMCC",
+  secreatekey:"sTPDtUMgiCGkiN7eP14nCnzxmUMI8LGt",
+  ipv4:"172.20.10.2",
+  macAddress:"5C-FB-3A-1D-C5-5D"
 };
